a68e1e040571f8af5c8a474237c7660400aa155f
shifinmst <shifint1000@gmail.com>
2026-06-04 16:50:22 +0530

    first commit

---
+++ string.py
@@ -0,0 +1,10 @@
+#d={"name": "jhon" , "age" : 30} 
+#d1={"a": 1}
+#d2= dict(b=2)
+#d.pop("age")
+#del d["name"]
+d = {
+"p1": {"name": "John", "age": 30},
+"p2": {"name": "Alice", "age": 25}
+}
+print(d["p2"]["name"])
