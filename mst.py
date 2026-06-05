c9e3fe36384c317b3c93c37839ffab39dbe39224
shifinmst <shifint1000@gmail.com>
2026-06-05 16:09:48 +0530

    first commit

---
+++ mst.py
@@ -0,0 +1,19 @@
+#if a > 0:
+ #   print("positive number")
+#elif a==0:
+    #print("zero")
+#elif a==-1:
+    #print("number is -1")
+#else:
+   # print ("negative mumber")
+
+#def add (a,b):
+ #   print(a+b)
+#add(2,3)
+
+def greet(name,age):
+    print(name,age)
+greet(age=20, name="snadeep")
+
+
+
